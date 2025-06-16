a3662b25086838f0733180735b304a8cc9f772c4
GilDev <gildev@gmail.com>
2025-06-17 01:25:21 +0200

    🔧 Add default keymap

--- keyboards/gdek/keymaps/default/keymap.c
+++ keyboards/gdek/keymaps/default/keymap.c
@@ -1,7 +1,9 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
+
+enum gdek_keymap_layers {
+    LAYER_BASE = 0,
+    LAYER_RAISE,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
@@ -18,13 +20,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤   │   │   ├───┘
       *                       └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x6_3(
-      QK_BOOT,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       QK_BOOT,     KC_7,     KC_8,    KC_9,    KC_0,    QK_BOOT,
-      QK_RGB_MATRIX_VALUE_UP,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       QK_RGB_MATRIX_VALUE_UP,     KC_U,     KC_I,    KC_O,    KC_P,    QK_RGB_MATRIX_VALUE_UP,
-      QK_RGB_MATRIX_VALUE_DOWN,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       QK_RGB_MATRIX_VALUE_DOWN,     KC_J,     KC_K,    KC_L,    KC_SCLN, QK_RGB_MATRIX_VALUE_DOWN,
-      DB_TOGG,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   DB_TOGG,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, DB_TOGG,
-                        KC_LCTL, KC_LGUI, KC_LCMD, KC_LALT, KC_ENT,    KC_SPC, KC_RALT,  KC_RCMD,  KC_RGUI, KC_RCTL
-    )
+    [LAYER_BASE] = LAYOUT(
+      KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+      KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+      /*KC_ESC*/KC_RBRC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   QK_BOOT,           QK_BOOT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(LAYER_RAISE),   KC_ENT,  KC_BSPC, KC_RALT, KC_RGUI, KC_RCTL
+    ),
+    [LAYER_RAISE] = LAYOUT(
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DB_TOGG,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RM_SATU, RM_HUEU, RM_NEXT, RM_SPDU, RM_VALU, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RM_SATD, RM_HUED, RM_PREV, RM_SPDD, RM_VALD, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_CUT,  KC_COPY, KC_PSTE, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                        _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
+    ),
 };
 
 void keyboard_post_init_user(void) {
